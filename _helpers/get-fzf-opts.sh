a393e4e954847db7e271d0db78129d406f955dde
Sumit Sahrawat <139506625+sumit-sahrawat@users.noreply.github.com>
2025-02-27 08:20:16 +0530

    feat(zsh): fzf wrapper with colors using whiskers

---
+++ _helpers/get-fzf-opts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -eu
+
+echo "{%- set palette = flavor.colors %}
+	--color=bg+:#{{palette.surface0.hex}}
+	--color=bg:#{{palette.base.hex}}
+	--color=fg+:#{{palette.text.hex}}
+	--color=fg:#{{palette.text.hex}}
+	--color=header:#{{palette.red.hex}}
+	--color=hl+:#{{palette.red.hex}}
+	--color=hl:#{{palette.red.hex}}
+	--color=info:#{{palette.mauve.hex}}
+	--color=marker:#{{palette.lavender.hex}}
+	--color=pointer:#{{palette.rosewater.hex}}
+	--color=prompt:#{{palette.mauve.hex}}
+	--color=selected-bg:#{{palette.surface1.hex}}
+	--color=spinner:#{{palette.rosewater.hex}}
+" | whiskers - -f "${1}" | xargs echo
