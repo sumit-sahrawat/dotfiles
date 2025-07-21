28279c5a238d9c197ea7c69334fc90945fd7f4c2
Sumit Sahrawat <139506625+sumit-sahrawat@users.noreply.github.com>
2025-07-21 12:19:42 +0530

    feat(neovim): disable animations

--- remove_dot_config/remove_nvim/lua/config/options.lua
+++ remove_dot_config/remove_nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable animations globally
+vim.g.snacks_animate = false
