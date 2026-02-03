c3cef82f9d5fa6b108ab4445dd9dbc54d58cfa0b
Sumit Sahrawat <sumit-sahrawat@noreply.codeberg.org>
2026-02-03 22:32:17 +0530

    feat(neovim): use slanted separators for lualine

---
+++ remove_dot_config/remove_nvim/lua/exact_plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = {
+          left = '',
+          right = '',
+        },
+        section_separators = {
+          left = '',
+          right = '',
+        },
+      },
+    },
+  },
+}
