5bcdf7c4432cc1765b529b56bd3355b5229ffab3
Sumit Sahrawat <sumit-sahrawat@noreply.codeberg.org>
2026-02-08 12:13:56 +0530

    feat(neovim): add toggle for nvim-autopairs

--- remove_dot_config/remove_nvim/lua/exact_plugins/autopair.lua
+++ remove_dot_config/remove_nvim/lua/exact_plugins/autopair.lua
@@ -2,7 +2,22 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {},
+    opts = function()
+      Snacks.toggle({
+        name = "Auto Pairs",
+        get = function()
+          return not require("nvim-autopairs").state.disabled
+        end,
+        set = function(state)
+          local m = require("nvim-autopairs")
+          if state then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map("<leader>up")
+    end,
   },
   {
     "nvim-mini/mini.pairs",
