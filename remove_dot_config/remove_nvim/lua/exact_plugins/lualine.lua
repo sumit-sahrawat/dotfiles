d052ed737478a4d8e6e1a12ede724db44a468a3f
Sumit Sahrawat <sumit-sahrawat@noreply.codeberg.org>
2026-02-11 15:46:33 +0530

    feat(neovim): remove separators

--- remove_dot_config/remove_nvim/lua/exact_plugins/lualine.lua
+++ remove_dot_config/remove_nvim/lua/exact_plugins/lualine.lua
@@ -3,14 +3,8 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
-        component_separators = {
-          left = '',
-          right = '',
-        },
-        section_separators = {
-          left = '',
-          right = '',
-        },
+        component_separators = "",
+        section_separators = "",
       },
     },
   },
