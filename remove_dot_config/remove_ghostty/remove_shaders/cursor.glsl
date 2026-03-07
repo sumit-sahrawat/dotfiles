911dad7870d15b816cf3e3804099f2131c8c4209
Sumit Sahrawat <sumit-sahrawat@noreply.codeberg.org>
2026-03-07 08:30:01 +0530

    feat(ghostty): snappier cursor trail

--- remove_dot_config/remove_ghostty/remove_shaders/cursor.glsl
+++ remove_dot_config/remove_ghostty/remove_shaders/cursor.glsl
@@ -1,10 +1,10 @@
 vec4 TRAIL_COLOR = iCurrentCursorColor; // can change to eg: vec4(0.2, 0.6, 1.0, 0.5);
-const float DURATION = 0.15; // total animation time
-const float TRAIL_SIZE = 0.8; // 0.0 = all corners move together. 1.0 = max smear (leading corners jump instantly)
+const float DURATION = 0.1; // total animation time
+const float TRAIL_SIZE = 1.0; // 0.0 = all corners move together. 1.0 = max smear (leading corners jump instantly)
 const float THRESHOLD_MIN_DISTANCE = 2.0; // min distance to show trail (units of cursor height)
 const float BLUR = 1.0; // blur size in pixels (for antialiasing)
-const float TRAIL_THICKNESS = 1.0;  // 1.0 = full cursor height, 0.0 = zero height, >1.0 = funky aah
-const float TRAIL_THICKNESS_X = 0.9;
+const float TRAIL_THICKNESS = 0.8;  // 1.0 = full cursor height, 0.0 = zero height, >1.0 = funky aah
+const float TRAIL_THICKNESS_X = 0.8;
 
 const float FADE_ENABLED = 1.0; // 1.0 to enable fade gradient along the trail, 0.0 to disable
 const float FADE_EXPONENT = 5.0; // exponent for fade gradient along the trail
